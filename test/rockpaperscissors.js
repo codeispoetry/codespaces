6f208ce02d64e69d2c9458d768ba387643d65106
Thomas Rose <t.rose@seidemann-web.com>
2022-06-28 13:14:49 +0200

    add rockpaperscissors

--- test/rockpaperscissors.js
+++ test/rockpaperscissors.js
@@ -1,27 +1,39 @@
 //https://www.codewars.com/kata/5672a98bdbdd995fad00000f/train/javascript
 
 const rps = (p1, p2) => {
-	return 1;
+	if (p1 === p2) return "Draw!";
+
+	var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+	if (p2 === rules[p1]) {
+		return "Player 1 won!";
+	}
+	else {
+		return "Player 2 won!";
+	}
 }
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
 
 describe('rock paper scissors', function() {
 const getMsg = (n) => `Player ${n} won!`;
 
 	it('player 1 win', function() {
-		Test.assertEquals(rps('rock', 'scissors'), getMsg(1));
-		Test.assertEquals(rps('scissors', 'paper'), getMsg(1));
-		Test.assertEquals(rps('paper', 'rock'), getMsg(1));
+		assert.equal(rps('rock', 'scissors'), getMsg(1));
+		assert.equal(rps('scissors', 'paper'), getMsg(1));
+		assert.equal(rps('paper', 'rock'), getMsg(1));
 	});
 
 	it('player 2 win', function() {
-		Test.assertEquals(rps('scissors', 'rock'), getMsg(2));
-		Test.assertEquals(rps('paper', 'scissors'), getMsg(2));
-		Test.assertEquals(rps('rock', 'paper'), getMsg(2));
+		assert.equal(rps('scissors', 'rock'), getMsg(2));
+		assert.equal(rps('paper', 'scissors'), getMsg(2));
+		assert.equal(rps('rock', 'paper'), getMsg(2));
 	});
 
 	it('draw', function() {
-		Test.assertEquals(rps('rock', 'rock'), 'Draw!');
-		Test.assertEquals(rps('scissors', 'scissors'), 'Draw!');
-		Test.assertEquals(rps('paper', 'paper'), 'Draw!');
+		assert.equal(rps('rock', 'rock'), 'Draw!');
+		assert.equal(rps('scissors', 'scissors'), 'Draw!');
+		assert.equal(rps('paper', 'paper'), 'Draw!');
 	});
 });
